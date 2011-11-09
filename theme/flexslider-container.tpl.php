283b6ca1d2f7f616be779543cbad8a7ee189cffc
Ryan Page <jepedo@gmail.com>
2011-11-09 15:34:12 -0500

    merged @minorOffense code with initial codebase

--- theme/flexslider-container.tpl.php
+++ theme/flexslider-container.tpl.php
@@ -1,45 +1,17 @@
 <?php
 /**
  * @file
- * Default output for a galleria node.
-
+ * Default output for a FlexSlider node.
 */
 ?>
 <div class="flexslider-content flexslider clearfix" id="flexslider-<?php print $id; ?>">
   <ul class="slides">
-  
-  <?php /*dpm($items);*/?>
-  
-  <?php foreach($items as $item) { ?>
+  <?php foreach($items as $item) : ?>
     <li><?php print render($item); ?>
-       
-
-
-  <?php if($item['#item']['title'] != '' || $item['#item']['alt'] != '') { ?>
+      <?php if(!empty($item['#item']['title']) || !empty($item['#item']['alt'])) : ?>
        <div class="flex-caption"><strong><?php print $item['#item']['title']; ?></strong>&nbsp;<?php print $item['#item']['alt'];?></div>
-  <?php } ?>     
- 
-  </li>
- 
-  <?php } ?>   
+      <?php endif; ?>
+    </li>
+  <?php endfor; ?>
   </ul>
-
 </div>
-
-
-
-
-<?php /* foreach($items as $item) if(!empty($item['#item']['alt'])) { 
-    <li>print $item['#item']['alt'];</li>
-   }
- ?>
-   <?php if(!empty($item['#item']['alt'])) {
-   
-   <?php if(strnlen($item['#item']['alt'])) > 0)
-   
-   <div class="flex-caption"><?php print $item['#item']['title'];?></div>
-
-<?php /*
-<div class="flex-caption"><li><?php print $item['#item']['alt']; ?></li></div>
-*/     
-?>
