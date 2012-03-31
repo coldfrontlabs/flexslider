8ee57b79c461a4cec8ce028641e72ab712aabb41
Mathew Winstone <mathew.winstone@gmail.com>
2012-03-31 13:11:40 -0400

    Fix for issue [#1415474] Fixed nav layering Added flex-nav-container element per the demo on flex slider homepage

--- flexslider_views_slideshow/theme/flexslider-views-slideshow-main-frame.tpl.php
+++ flexslider_views_slideshow/theme/flexslider-views-slideshow-main-frame.tpl.php
@@ -7,8 +7,10 @@
  * @author Mathew Winstone (minorOffense) <mwinstone@coldfrontlabs.ca>
  */
 ?>
-<div class="flexslider">
-  <ul id="flexslider_views_slideshow_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>">
-    <?php print $rendered_rows; ?>
-  </ul>
+<div class="flex-nav-container">
+  <div class="flexslider">
+    <ul id="flexslider_views_slideshow_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>">
+      <?php print $rendered_rows; ?>
+    </ul>
+  </div>
 </div>
