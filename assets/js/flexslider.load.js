a25219dd72750bc657566a8397a4ac09c2bb8c86
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2012-08-20 17:59:27 -0400

    Adjusted load script to load for any defined id of a flex slider container

--- assets/js/flexslider.load.js
+++ assets/js/flexslider.load.js
@@ -3,24 +3,24 @@
 // Behavior to load FlexSlider
 Drupal.behaviors.flexslider = {
   attach: function(context, settings) {
-    $('.flexslider', context).once('flexslider', function() {
-      $(this).each(function() {
-        var $this = $(this);
-        var id = $this.attr('id');
-        if (settings.flexslider !== undefined) {
-          var optionset = settings.flexslider.instances[id];
+    for (id in settings.flexslider.instances) {
+      $('#' + id, context).once('flexslider', function() {
+        if (settings.flexslider.optionsets[settings.flexslider.instances[id]] !== undefined) {
+          // Remove width/height attributes
+          // @todo load the css path from the settings
+          $(this).find('ul.slides > li > img').removeAttr('height');
+          $(this).find('ul.slides > li > img').removeAttr('width');
+          
+          var optionset = settings.flexslider.optionsets[settings.flexslider.instances[id]];
           if (optionset) {
-            $this.flexslider(settings.flexslider.optionsets[optionset]);
+            $(this).flexslider(optionset);
           }
           else {
-            $this.flexslider();
+            $(this).flexslider();
           }
         }
       });
-      // Remove width/height attributes
-      $(this).find('ul.slides li img').removeAttr('height');
-      $(this).find('ul.slides li img').removeAttr('width');  
-    });
+    }
   }
 };
 
