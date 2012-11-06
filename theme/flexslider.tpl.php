77b9d8db85a9e3015b92975e88c1315fa9c8dd3c
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2012-11-06 16:16:44 -0500

    Removed old code

--- theme/flexslider.tpl.php
+++ theme/flexslider.tpl.php
@@ -1,39 +1,10 @@
 <?php
 /**
  * @file
- * Default output for a FlexSlider node.
+ * Default output for a FlexSlider object.
 */
 ?>
 <div <?php print drupal_attributes($settings['attributes'])?>">
   <?php print theme('flexslider_list', array('items' => $items, 'settings' => $settings)); ?>
 </div>
 <?php
-/*
-<ul class="slides">
-<?php foreach($items as $item) : ?>
-  <li>
-    <?php
-      if (isset($item['#item']['image'])) {
-        print $item['#item']['image'];
-      }
-      else {
-        print render($item);
-      }
-    ?>
-    <?php if(isset($item['#item']['title']) || isset($item['#item']['alt'])) : ?>
-      <div class="flex-caption">
-        <?php if(!empty($item['#item']['title'])) : ?>
-          <strong><?php print $item['#item']['title']; ?></strong>
-        <?php endif; ?>
-        <?php if(!empty($item['#item']['alt'])) : ?>
-          <?php print $item['#item']['alt']; ?>
-        <?php endif; ?>
-      </div>
-    <?php endif; ?>
-  </li>
-<?php endforeach; ?>
-</ul>
-
-
-
-*/
